02d13a572bb3453f7fe6f89f1c900bce1c32e81e
Helen <howareyouhelen@hotmail.com>
2017-08-03 20:23:44 +0000

    added login and register functionality on to navbar

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CategoriesController < ApplicationController
     def index
-        @categories = Category.order(id: :desc).all
+        @categories = Category.order(name: :asc).all
     end
 
     def new
